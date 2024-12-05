551b16b572a24c3cfbb37e831af514294ec52525
Petr Brzek <petrbrzek@gmail.com>
2024-12-05 20:43:52 +0100

    fix type

--- components/plate-editor/handlers/html-to-text.ts
+++ components/plate-editor/handlers/html-to-text.ts
@@ -1,55 +1,55 @@
 export function htmlElementToText(element: HTMLElement) {
-  let text = "";
-  for (const child of element.childNodes) {
+  let text = ''
+  for (const child of Array.from(element.childNodes)) {
     switch (child.nodeType) {
       case Node.ELEMENT_NODE:
-        text += handleElement(child as HTMLElement);
-        break;
+        text += handleElement(child as HTMLElement)
+        break
       case Node.TEXT_NODE:
-        text += child.nodeValue;
-        break;
+        text += child.nodeValue
+        break
       default:
-        break;
+        break
     }
   }
-  return text;
+  return text
 }
 
 // Handle specific HTML elements for text formatting
 function handleElement(elem: HTMLElement) {
-  let text = "";
+  let text = ''
   switch (elem.tagName.toLowerCase()) {
-    case "p":
-    case "div":
-    case "br":
-      text += htmlElementToText(elem) + "\n";
-      break;
-    case "li":
-      text += "- " + htmlElementToText(elem) + "\n";
-      break;
-    case "ul":
-    case "ol":
-      text += htmlElementToText(elem) + "\n";
-      break;
-    case "h1":
-    case "h2":
-    case "h3":
-    case "h4":
-    case "h5":
-    case "h6":
-      text += htmlElementToText(elem) + "\n";
-      break;
-    case "b":
-    case "strong":
-      text += "*" + htmlElementToText(elem) + "*";
-      break;
-    case "i":
-    case "em":
-      text += "_" + htmlElementToText(elem) + "_";
-      break;
+    case 'p':
+    case 'div':
+    case 'br':
+      text += htmlElementToText(elem) + '\n'
+      break
+    case 'li':
+      text += '- ' + htmlElementToText(elem) + '\n'
+      break
+    case 'ul':
+    case 'ol':
+      text += htmlElementToText(elem) + '\n'
+      break
+    case 'h1':
+    case 'h2':
+    case 'h3':
+    case 'h4':
+    case 'h5':
+    case 'h6':
+      text += htmlElementToText(elem) + '\n'
+      break
+    case 'b':
+    case 'strong':
+      text += '*' + htmlElementToText(elem) + '*'
+      break
+    case 'i':
+    case 'em':
+      text += '_' + htmlElementToText(elem) + '_'
+      break
     default:
-      text += htmlElementToText(elem);
-      break;
+      text += htmlElementToText(elem)
+      break
   }
-  return text;
+  return text
 }
